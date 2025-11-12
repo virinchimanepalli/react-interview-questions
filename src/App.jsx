7dc292ff781ee5881c61cd8f9f943d60a910fc66
virinchimanepalli <virinchi.msk83@gmail.com>
2025-11-12 14:10:58 +0530

    remove everything

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import './App.scss';
 import './styles/styles.css';
-import Autocomplete from './components/AutoComplete';
+import Autocomplete from './components/AutoCompleteV3';
+
+const END_POINT = 'https://rickandmortyapi.com/api/character';
 
 function App() {
+  const onSelectItem = (value) => {
+    console.log(value, 'value');
+  };
+
   return (
     <>
-      <div>Hello world</div>
+      <div>Hello world - v3</div>
 
-      <Autocomplete />
+      <Autocomplete
+        onSelectItem={onSelectItem}
+        placeholder="search..."
+        endpoint={END_POINT}
+        delay={200}
+        minChars={0}
+      />
     </>
   );
 }
