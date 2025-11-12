47c8b34b5168624dce5c318e7c3e5c58e85c081b
virinchimanepalli <virinchi.msk83@gmail.com>
2025-11-12 15:21:28 +0530

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import './App.scss';
 import './styles/styles.css';
-import Autocomplete from './components/AutoCompleteV3';
-
-const END_POINT = 'https://rickandmortyapi.com/api/character';
 
 function App() {
-  const onSelectItem = (value) => {
-    console.log(value, 'value');
-  };
-
   return (
     <>
       <div>Hello world - v3</div>
-
-      <Autocomplete
-        onSelectItem={onSelectItem}
-        placeholder="search..."
-        endpoint={END_POINT}
-        delay={200}
-        minChars={0}
-      />
     </>
   );
 }
